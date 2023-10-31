2269986ad020587eb6e0c34896d878c1e93fc7f1
Asmaly7 <148426887+Asmaly7@users.noreply.github.com>
2023-10-31 17:22:40 +0900

    cardbackcolor

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,6 +7,8 @@ public class Card : MonoBehaviour
     public Animator cardAnim;
     public AudioClip flip;
     public AudioSource audioSource;
+    public Color flippedColor;             // 뒤집힌 상태의 색상
+    private bool isFlipped = false;        // 카드가 뒤집힌 상태 여부
 
     public int Number;
 
@@ -37,6 +39,12 @@ public class Card : MonoBehaviour
         
         transform.Find("front").gameObject.SetActive(true);
         transform.Find("back").gameObject.SetActive(false);
+        if (!isFlipped)
+        {
+            // 카드가 한 번 이상 뒤집힌 경우에만 색상 변경
+            isFlipped = true;
+            ChangeCardColor(Color.gray);
+        }
         cardAnim.SetBool("isOpen", true);
     }
     public void destroyCard()
@@ -59,5 +67,12 @@ public class Card : MonoBehaviour
         cardAnim.SetBool("isOpen", false);
         transform.Find("back").gameObject.SetActive(true);
         transform.Find("front").gameObject.SetActive(false);
+    }
+    private void ChangeCardColor(Color newColor)
+    {
+        // 카드의 뒷면 스프라이트의 색상 변경
+        SpriteRenderer backSprite = transform.Find("back").GetComponent<SpriteRenderer>();
+        backSprite.color = newColor;
+
     }
 }
